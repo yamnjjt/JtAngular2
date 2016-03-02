06f0f20083caf986a7dfbfd806015767f2415959
Jungtae JI <yamnjjt@hotmail.com>
2016-03-03 01:49:03 +1000

    initial commit

---
+++ src/JtAngular2/gulpfile.js
@@ -0,0 +1,42 @@
+﻿/// <binding AfterBuild='moveToLibs, moveToApps' Clean='clean' />
+
+var gulp = require('gulp');
+var del = require('del');
+
+
+var paths = {
+    npmSrc: "./node_modules/",
+    libTarget: "./wwwroot/libs/",
+    tsSrc: "./scripts/",
+    appTarget: "./wwwroot/app/"
+};
+
+
+var libsToMove = [
+   paths.npmSrc + '/angular2/bundles/angular2-polyfills.js',
+   paths.npmSrc + '/systemjs/dist/system.src.js',
+   paths.npmSrc + '/systemjs/dist/system-polyfills.js',
+   paths.npmSrc + '/rxjs/bundles/Rx.js',
+   paths.npmSrc + '/angular2/bundles/angular2.dev.js',
+   paths.npmSrc + '/angular2/bundles/router.dev.js',
+   paths.npmSrc + '/es6-shim/es6-shim.min.js',
+   paths.npmSrc + '/angular2/es6/dev/src/testing/shims_for_IE.js'
+];
+
+var appsToMove = [
+    paths.tsSrc + '*.html',
+    paths.tsSrc + '*.css'
+];
+
+gulp.task('moveToLibs', function () {
+    return gulp.src(libsToMove).pipe(gulp.dest(paths.libTarget));
+});
+
+
+gulp.task('moveToApps', function () {
+    return gulp.src(appsToMove).pipe(gulp.dest(paths.appTarget));
+});
+
+gulp.task('clean', function () {
+    return del([paths.libTarget, paths.appTarget]);
+});
---
+++ src/JtAngular2/scripts/app.component.ts
@@ -0,0 +1,38 @@
+﻿import { Component }        from 'angular2/core';
+import { RouteConfig, ROUTER_DIRECTIVES, ROUTER_PROVIDERS } from 'angular2/router';
+import { HeroService }      from './hero.service';
+import { HeroesComponent }  from './heroes.component';
+import { HeroDetailComponent }  from './hero-detail.component'
+import { DashboardComponent}    from './dashboard.component';
+
+@Component({
+    selector: 'my-app',
+    templateUrl: './app/app.component.html',
+    styleUrls: ['./app/app.component.css'],
+    directives: [ROUTER_DIRECTIVES],
+    providers: [HeroService, ROUTER_PROVIDERS]
+})
+
+@RouteConfig([
+    {
+        path: '/heroes',
+        name: 'Heroes',
+        component: HeroesComponent
+    },
+    {
+        path: '/dashboard',
+        name: 'Dashboard',
+        component: DashboardComponent,
+        useAsDefault: true
+    },
+    {
+        path: '/detail/:id',
+        name: 'HeroDetail',
+        component: HeroDetailComponent
+    }
+])
+
+
+export class AppComponent {
+    title = 'Tour of Heroes';
+}
